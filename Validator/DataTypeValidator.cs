178432defa99a543047ef4a6ed3b407b311a983f
Simon Hylleberg Christensen <simo675z@herningsholm.dk>
2020-10-21 16:34:26 +0200

    Lavet validator CL

---
+++ Validator/DataTypeValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Validator
+{
+    class DataTypeValidator
+    {
+        public bool checkIntInput(string input)
+        {
+            int parsedVal;
+            return int.TryParse(input, out parsedVal);
+        }
+        public bool checkDateTimeInput(string input)
+        {
+            DateTime parsedDateTime;
+            return DateTime.TryParse(input, out parsedDateTime);
+        }
+    }
+}
---
+++ Validator/InstrumentValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Validator
+{
+    public class InstrumentValidator
+    {
+        public bool CheckEmailFormat(string email)
+        {
+            return Regex.IsMatch(email, @"\A(?:[a-z0-9#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase); ;
+        }
+        public bool CheckDateTime(DateTime date)
+        {
+            return date > new DateTime(1880, 1, 1) && date < DateTime.Now;
+        }
+        public bool CheckStringInput(string str)
+        {
+            return !string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
+        }
+        public bool CheckSpecificSymbols(string str, List<char> symbols)
+        {
+            return str.Any(strChar => symbols.Any(symbolChar => symbolChar == strChar));
+        }
+        public bool CheckNotLetterOrDigit(string str)
+        {
+            return str.Any(ch => !char.IsLetterOrDigit(ch));
+        }
+    }
+}
